7c6107a7a49ed3f6885e56cb46a9ff1550f67e04
Forma500 <forma500@tftic.be>
2023-05-19 17:04:38 +0200

    Fin de la demo :)

---
+++ Demo_ADO.App/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+
+// ConnectionString 
+string connectionString = @"Server=Forma500\TFTIC;Database=Demo_ADO;User Id=Della;Password=Test1234=;TrustServerCertificate=true;";
+
+// Nuget package à installer → Microsoft.Data.SqlClient
+
+#region Connnexion vers la DB
+// - Instance d'une connexion + ConnectionString
+SqlConnection demoConnection = new SqlConnection();
+demoConnection.ConnectionString = connectionString;
+
+// - Ouvrir la connexion
+demoConnection.Open();
+
+// - Réaliser du traitement...
+Console.WriteLine($"Etat de la connexion {demoConnection.State}");
+
+// - Exemple de requete
+SqlCommand demoCommand = demoConnection.CreateCommand();
+demoCommand.CommandText = "SELECT COUNT(*) FROM [V_Game];";
+demoCommand.CommandType = System.Data.CommandType.Text;
+
+int nbGame = (int)demoCommand.ExecuteScalar();
+Console.WriteLine($"Le nombre de jeu dans la DB : {nbGame}");
+
+
+// - Fermer la connexion
+demoConnection.Close();
+Console.WriteLine($"Etat de la connexion {demoConnection.State}");
+#endregion
---
+++ Demo_ADO.DB/Views/V_Game.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW [dbo].[V_Game]
+AS 
+SELECT [Id_Game], [Name], [Id_Publisher], [Release_Date], [Price], [Resume]
+FROM [Game]
+WHERE [Is_Active] = 1
